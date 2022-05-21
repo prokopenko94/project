e418c4860db9bf372a9310b8108e32c816afc198
Danil <prokopps4prokop@gmail.com>
2022-05-21 13:34:48 +0300

    first commit

---
+++ studies/script.js
@@ -0,0 +1,161 @@
+'use strict';
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// {
+//     var result = 'Alex';
+// }
+
+// console.log(result);
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+////
+
+
+// console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+
+// ['vruru', 'ecrvrev', 'vvuguegv'].map(a => )
+
+// Request 
+// data
+// response
+
+// snake_case  <-- один из вариантов названия папок и файлов, но не переменных в js
+// UPPER_SNAKE_CASE  <-- программисты так обозначают переменные, которые хотят видеть в виде const(констант)
+// Kebab-case  <-- один из вариантов названия папок и файлов , но не переменных в js
+// PascalCase <-- используется для названия классов в js
+
+// const COLOR_RED = '#F00';
+
+
+// let userName = 'John';
+// let userNumber = 25;
+// userNumber = 2
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = `5`;
+
+// const bool = false;
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj['name']);
+
+//             0           1           2   3            4   5
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+
+// const arr = ['a', 'b', 'c'];
+
+// const arrObj = {
+//     a: 'a',
+//     '1': 'b',
+//     2: 'c',
+//     abc: {
+//         df: [{},{}],
+//         def: {
+
+//         }
+//     }
+
+// };
+// const b = 'b';
+
+// arrObj.b = '1234';
+// arrObj[b] = ['1234'];
+
+// console.log(arrObj[b]);
+// console.log(arrObj.b);
+
+// const obj = {a: 1, b: 2};
+
+// const obj = {
+//     Anna: 500,
+//     'Alice': 800
+// };
+
+
+
+// const storeName = 'redux';
+
+// const storeDescription = {
+//     budget: 10000,
+//     employess: ['Alex', 'Daniel', 'John'],
+//     products: {
+//         iphone: 2000,
+//         macbook:5000
+//     },
+//     open: true
+// };
+
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('вам есть 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваше фамилия?', '');
+// answers[3] = prompt('сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked || !isClose);
+
+// console.log(2 + 2 * 2 != '6');
